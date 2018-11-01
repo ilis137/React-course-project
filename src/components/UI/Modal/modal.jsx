c1199b3995993703baef2d3ac8a7d494f067684f
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-01 18:52:02 +0530

    lifecycle hooks added in modal

--- src/components/UI/Modal/modal.jsx
+++ src/components/UI/Modal/modal.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./modal.module.css";
 import BackDrop from "../BackDrop/BackDrop";
-const modal = props => {
-  return (
-    <>
-      <BackDrop show={props.show} click={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-      >
-        {props.children}
-      </div>
-    </>
-  );
-};
-export default modal;
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.show !== nextProps.show;
+  }
+  componentWillUpdate() {
+    console.log("component will update");
+  }
+  render() {
+    return (
+      <>
+        <BackDrop show={this.props.show} click={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
+export default Modal;
