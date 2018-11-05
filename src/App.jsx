613f78c44d1168c27f9bf6f645cd5112327358ad
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-05 18:49:33 +0530

    checkout container added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import "./App.css";
-
+import Checkout from "./containers/Checkout/Checkout";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
