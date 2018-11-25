db4762cbfb179884c050b7b7531737c0d1e9621a
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-25 18:18:41 +0530

    add sign in

--- src/Store/actions/auth.jsx
+++ src/Store/actions/auth.jsx
@@ -1,15 +1,16 @@
 import * as actionTypes from "./actions";
-
+import axios from "../../Axios-orders";
 export const authStart = () => {
   return {
     type: actionTypes.AUTH_START
   };
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (idToken, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    token: idToken,
+    userId
   };
 };
 export const authFail = error => {
@@ -19,8 +20,26 @@ export const authFail = error => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+    let url =
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDsa3CzDo7uVMf_y53qcMioD9M8FjBlxkU";
+    if (!isSignUp) {
+      url =
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDsa3CzDo7uVMf_y53qcMioD9M8FjBlxkU";
+    }
+    axios
+      .post(url, authData)
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data.idToken, res.data.localId));
+      })
+      .catch(error => dispatch(authFail(error)));
   };
 };
