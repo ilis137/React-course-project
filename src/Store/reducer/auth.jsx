227535b9115adf2cec79ab5f3b90f54b7262f21e
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-25 18:20:01 +0530

    add reducers

---
+++ src/Store/reducer/auth.jsx
@@ -0,0 +1,32 @@
+import * as actionTypes from "../actions/actions";
+import { updateObject } from "../utility";
+const initialState = {
+  token: null,
+  userId: null,
+  loading: false,
+  error: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.AUTH_START:
+      return updateObject(state, { error: null, loading: true });
+    case actionTypes.AUTH_SUCCESS:
+      return updateObject(state, {
+        loading: false,
+        token: action.token,
+        userId: action.userId,
+        error: null
+      });
+    case actionTypes.AUTH_FAIL:
+      return updateObject(state, {
+        loading: false,
+        token: null,
+        userId: null,
+        error: action.error
+      });
+    default:
+      return state;
+  }
+};
+export default reducer;
